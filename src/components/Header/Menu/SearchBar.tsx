6a6b68c3fa790ef52dc20c89061b065692284bf0
EseAlli <essealli.ea@gmail.com>
2023-03-11 11:10:56 +0100

    created card component and added font

--- src/components/Header/Menu/SearchBar.tsx
+++ src/components/Header/Menu/SearchBar.tsx
@@ -3,7 +3,7 @@ import { MdSearch } from "react-icons/md";
 
 const SearchBar = () => {
   return (
-    <div className="flex items-center md:border rounded-md md:bg-slate-200">
+    <div className="flex items-center md:border rounded-md md:bg-[#f2f2f2]">
       <input
         className="py-2 px-3 text-gray-700 leading-tight focus:outline-none hidden md:block bg-transparent"
         type="text"
