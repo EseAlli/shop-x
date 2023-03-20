8598d5bfe9b7e9ee4f58361999eff2f89c08b286
EseAlli <essealli.ea@gmail.com>
2023-03-20 23:47:08 +0100

    modal skeleton added

--- src/components/Auth/index.tsx
+++ src/components/Auth/index.tsx
@@ -1,8 +1,40 @@
-import Modal from "@components/Modal";
-import React from "react";
+import { FC, ReactNode } from "react";
 
-const index = () => {
-  return <Modal />;
+type ModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+};
+
+const index: FC<ModalProps> = ({ isOpen, onClose, children }) => {
+  return (
+    <>
+      {isOpen && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
+          <div className="relative w-auto max-w-3xl mx-auto my-6">
+            <div className="relative flex flex-col w-full bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none">
+              <div className="flex items-start justify-between p-5 border-b border-solid rounded-t border-blueGray-200">
+                <h3 className="text-3xl font-semibold">{/* Modal Title */}</h3>
+                <button
+                  className="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                  onClick={onClose}
+                >
+                  <span className="bg-transparent text-black h-6 w-6 text-2xl block outline-none focus:outline-none">
+                    ×
+                  </span>
+                </button>
+              </div>
+              <div className="relative p-6 flex-auto">{children}</div>
+            </div>
+          </div>
+          <div
+            className="fixed inset-0 z-40 bg-black opacity-50"
+            onClick={onClose}
+          ></div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default index;
