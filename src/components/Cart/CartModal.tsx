5018e4d96ed8609a8e94d3c3eb77bb4097b8db7f
EseAlli <essealli.ea@gmail.com>
2023-04-20 01:05:46 +0100

    cart implementation in progress

--- src/components/Cart/CartModal.tsx
+++ src/components/Cart/CartModal.tsx
@@ -1,5 +1,12 @@
 import Modal from "@components/Modal";
-import React, { FC, useContext, useState } from "react";
+import React, {
+  FC,
+  useCallback,
+  useContext,
+  useState,
+  useRef,
+  ChangeEvent,
+} from "react";
 import Link from "next/link";
 import { BsArrowRight } from "react-icons/bs";
 import { CartContext } from "context/cartContext";
@@ -14,6 +21,28 @@ type Props = {
 const CartModal: FC<Props> = ({ isOpen, onClose }) => {
   const { currentItem } = useContext(CartContext) as CartContextType;
   const [currentImage, setCurrentImage] = useState(currentItem?.images[0]);
+  const [item, setItem] = useState<any>({ ...currentItem });
+  const [size, setSize] = useState<string>("");
+  const sizeRef = useRef(null);
+  const addItemToCart = useCallback(() => {
+    if (size === "") {
+      //@ts-ignore
+      sizeRef?.current?.focus();
+    } else if (size) {
+      //@ts-ignore
+      sizeRef?.current?.blur();
+      setItem({ ...currentItem, size });
+      console.log(item);
+    }
+  }, [size, currentItem]);
+
+  const handleSizeChange = useCallback(
+    (e: any) => {
+      setSize(e.target.value);
+      addItemToCart();
+    },
+    [addItemToCart]
+  );
   return (
     <>
       <Modal title="" onClose={onClose} isOpen={isOpen}>
@@ -22,7 +51,7 @@ const CartModal: FC<Props> = ({ isOpen, onClose }) => {
             <div>
               <Image
                 //@ts-ignore
-                src={currentImage}
+                src={currentImage || currentItem?.images[0]}
                 height={600}
                 width={400}
                 alt={`${currentItem?.name}`}
@@ -69,16 +98,27 @@ const CartModal: FC<Props> = ({ isOpen, onClose }) => {
             </div>
 
             <div className="flex flex-col gap-y-3 mt-3 w-5/6">
-              <select className="border border-black p-2 mb-1">
-                <option>Select size</option>
+              <select
+                className="border border-black p-2 mb-1"
+                ref={sizeRef}
+                onFocus={(e) => (e.target.size = 6)}
+                onChange={(e) => handleSizeChange(e)}
+                onBlur={(e) => (e.target.size = 0)}
+              >
+                <option value="defaultsize">Select size</option>
                 <option>XS</option>
                 <option>SM</option>
                 <option>MD</option>
               </select>
 
-              <button className="bg-black text-white py-3">Add to cart</button>
+              <button
+                className="bg-black text-white py-3"
+                onClick={() => addItemToCart()}
+              >
+                Add to cart
+              </button>
               <Link
-                href={`p/${currentItem?.id}`}
+                href={`/p/${currentItem?.id}`}
                 className="self-end text-sm font-semibold flex items-center gap-1"
               >
                 More details <BsArrowRight />
